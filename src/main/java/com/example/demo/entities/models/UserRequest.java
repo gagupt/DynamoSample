99c3f5d34e2cd73da147d2b23a9d7c70a3cb46e7
Gaurav <guptagaurav890@gmail.com>
2018-09-10 00:05:13 +0530

    connecting to local dynamoDb

--- src/main/java/com/example/demo/entities/models/UserRequest.java
+++ src/main/java/com/example/demo/entities/models/UserRequest.java
@@ -4,7 +4,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
-@DynamoDBTable(tableName = "User")
+@DynamoDBTable(tableName = "UserNew")
 public class UserRequest {
 	private int id;
 	private String name;
